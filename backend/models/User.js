1df50c537fedda5f76fb47e7d2d6f03643b72b5f
shrunalibamane <shrunalibamane2005@gmail.com>
2025-02-18 10:04:12 +0530

    sayali

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,6 @@
-const mongoose= require('mongoose');
-const userSchema= new mongoose.Schema({
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
     email:{
         type:String,
         required:true
@@ -16,10 +17,8 @@ const userSchema= new mongoose.Schema({
         type:String,
         required:true
     }
-
 })
 
 const User = mongoose.model('User',userSchema);
-
 
 module.exports = {User};
